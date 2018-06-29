12a2078626b84ef091b861a2784915e7005c8403
mlapo96 <mflapoin@ucsc.edu>
2018-06-28 21:46:37 -0700

    changing stuff to make sure it works

--- web2py/applications/Raynime/controllers/default.py
+++ web2py/applications/Raynime/controllers/default.py
@@ -7,7 +7,7 @@
 # ---- example index page ----
 def index():
     response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('This is the homepage!'))
 
 # ---- API (example) -----
 @auth.requires_login()
