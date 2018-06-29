bc3e83fbd2f009e9c8e0dff2af8a2de73ae2ac95
mlapo96 <mflapoin@ucsc.edu>
2018-06-29 00:52:23 -0700

    pull

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8c9041614ed23ebd$d585a866eb904b839dd2ba309df74cd5d327a775"
+password="pbkdf2(1000,20,sha512)$9e271f5566b164f1$6cedbab0e4d4adacdef9e2372bb6c48a0aeaad86"
