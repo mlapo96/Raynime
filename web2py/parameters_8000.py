c94464a64be70ea198f35ff1053b40a4f8d32719
mlapo96 <mflapoin@ucsc.edu>
2018-06-29 21:35:48 -0700

    chacha

--- web2py/parameters_8000.py
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$ad3440bc88f86c4f$fa61bbbadd5fb9a9db2b6292c37d85dce01d1674"
-=======
-password="pbkdf2(1000,20,sha512)$9714eb394687fa25$dbc203bb8d06d4b50f8cb69fc4df5bae5f491ecd"
->>>>>>> branch 'master' of https://github.com/mlapo96/website
